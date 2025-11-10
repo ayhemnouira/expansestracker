61ed05329444433596969766b71d203b64ac8643
ayhemnouira <ayhemnouira9@gmail.com>
2025-11-10 14:41:43 +0100

    .

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // Loads .env automatically
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
