47235dce90581644225ba24288adeba06eb4225f
ayhemnouira <ayhemnouira9@gmail.com>
2025-11-25 20:11:46 +0100

    docs: add application description comment

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,5 +1,5 @@
 package com.example.backend;
-
+// ExpensesTracker - Personal Finance Management Application
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
